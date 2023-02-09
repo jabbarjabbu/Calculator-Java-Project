ad5da933be91d1874a10392049e0cd85599c5a60
jabba <jabbu0433@gmail.com>
2023-02-09 20:08:22 +0530

    calculator-commit

---
+++ Calculator/src/com/multiplication/Multiplication.java
@@ -0,0 +1,9 @@
+package com.multiplication;
+
+public class Multiplication {
+
+	public static float multiplication(float a, float b)
+	{
+		return a*b;
+	}
+}
---
+++ Calculator/src/com/subtraction/Subtraction.java
@@ -0,0 +1,9 @@
+package com.subtraction;
+
+public class Subtraction {
+
+	public static float subtraction(float a, float b)
+	{
+		return a-b;
+	}
+}
